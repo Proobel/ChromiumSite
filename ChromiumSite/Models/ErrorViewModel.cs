0ab7390fdd955bad7f5eb6fbf01e29b2cf94b74d
Proobel <Geroi_6@bk.ru>
2018-02-18 02:06:55 +0300

    Добавьте файлы проекта.

---
+++ ChromiumSite/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ChromiumSite.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
